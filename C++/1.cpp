e7ac59b0230f081e01b948612dffc2f9832b0b7d
Your Name <you@example.com>
2021-05-12 13:37:43 +0530

    smilly

---
+++ C++/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+ cout<<"This is my first C++ Program.";
+ cout<<endl<<"and its very easy to learn";
+}
---
+++ proj/smilly.c
@@ -0,0 +1,17 @@
+/* C Program - Print Smiling Face on Screen */
+		
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int sml=1, i, limit, d=46;   // ASCII code for smiling face is 1. 
+	printf("How many smiley face you want to print ? ");
+	scanf("%d",&limit);
+	for(i=1; i<limit+1; i++)
+	{
+        printf("%d",i);
+        printf("%c",d);
+		printf("%c ",sml);
+	}
+}
