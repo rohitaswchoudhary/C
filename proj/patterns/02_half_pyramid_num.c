14950b9e2d8ef97f48470af9285c8d3d4fe5dc95
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2021-06-08 18:07:22 +0530

    added patterns programs

---
+++ proj/patterns/02_half_pyramid_num.c
@@ -0,0 +1,18 @@
+// 2021-06-07 12:13:36
+
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; ++i)
+    {
+        for (j = 1; j <= i; ++j)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ proj/patterns/06_half_pyramid.c
@@ -0,0 +1,32 @@
+// 2021-06-07 22:26:56
+
+/*
+example:
+
+Enter the number of rows: 5
+        *
+      ***
+    *****
+  *******
+*********
+
+*/
+
+#include <stdio.h>
+int main() {
+   int i, space, rows, k = 0; 
+   char ch = 2;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i, k = 0) {
+      for (space = 1; space <= rows - i; ++space) {
+         printf("   ");
+      }
+      while (k != 2 * i - 1) {
+         printf("*");
+         ++k;
+      }
+      printf("\n");
+   }
+   return 0;
+}
