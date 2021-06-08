753a72056277a753e6ce2bda52effd6c988c5c94
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2021-06-08 23:47:13 +0530

    added pattern2 with some more pattern examples

---
+++ proj/patterns/11_floid_tri.c
@@ -0,0 +1,21 @@
+// 2021-06-08
+
+// pattern:  Floyd's Triangle.
+
+#include <stdio.h>
+int main()
+{
+    int rows, i, j, number = 1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; ++j)
+        {
+            printf("%d ", number);
+            ++number;
+        }
+        printf("\n");
+    }
+    return 0;
+}
